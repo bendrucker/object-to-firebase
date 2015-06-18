204873d6db8fe8797dd94de3c7395216cda798d7
Ben Drucker <bvdrucker@gmail.com>
2015-06-18 19:36:24 +0200

    slim src by recursive process call

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ var valueToFirebase = require('value-to-firebase')
 var traverse = require('traverse')
 
 module.exports = function objectToFirebase (object) {
-  return traverse(object).map(function () {
-    process.call(this)
-    this.post(process)
-  })
+  return traverse(object).map(process)
 }
 
 function process () {
+  this.post(process)
   var value = valueToFirebase(this.node)
   if (this.isRoot) return this.update(value)
   if (value === null) return this.remove()
